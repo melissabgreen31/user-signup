d766a8eeec0748a439fad0525fb8e4024a68991d
Melissa Green <melissabgreen31@gmail.com>
2017-06-13 16:33:22 -0500

    User-Signup

---
+++ main.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+
+app = Flask(__name__)
+app.config['DEBUG']=True
+
+form= """
+<style>
+    
+</style>
+<h1>Signup</h1>
+        <form action ="/validate_info" method="post">
+        
+        <label for="user-signup">
+        Username:
+            <input type="text" name="username" value = '{username}'/>
+            <id="error">{username_error}
+            <br>
+        Password:
+            <input type="text" name="password"/> <id="error"/>{password_error}
+            
+            <br>
+        Verify Password:
+            <input type="text" name="confirmation"  />
+            <id="error">{confirmation_error}
+            <br>
+        Email(optional):
+            <input type="text" name="email" value = '{email}'/>
+            <id="error">{email_error}
+            <br>
+            <input type="submit" value ="Submit"/>
+        </label>
+        
+        </form>
+        """
+
+
+@app.route("/")
+def index():
+    template = jinja_env.get_template('form.html')
+    return template.render()
+
+@app.route('/validate_info', methods= ['POST'])
+def find_errors():
+    username = request.form['username']
+    password = request.form['password']
+    confirmation = request.form['confirmation']
+    email = request.form['email']
+
+    username_error = ''
+    password_error = ''
+    confirmation_error = ''
+    email_error = ''
+
+    if len(username) > 20 or len(username) <3: 
+        username_error = "That's not a valid username"
+        usename = ''
+        
+    
+    if len(password) > 20 or len(password) < 3:
+        password_error = "That's not a valid password"
+        
+
+    if not confirmation == password or len(confirmation) >20 or len(confirmation) < 3:
+        confirmation_error = "Passwords do not match"
+
+    if '@' and '.' not in email:
+       email_error = "Invalid Email"
+       email = ''
+        
+
+    if not username_error and not password_error and not confirmation_error and not email_error:
+        
+        return redirect('/welcome')
+    else:
+        return form.format(username_error = username_error, password_error = password_error, confirmation_error = confirmation_error, email_error = email_error, username= username, email= email)
+
+    
+
+@app.route('/welcome')
+def welcome():
+    template = jinja_env.get_template('welcome.html')
+    return template.render()
+
+    
+   
+
+
+app.run()
